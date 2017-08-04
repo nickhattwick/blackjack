e18f26288b0110787525b90ef036d0779ce2188a
Nick <nhattwick@gmail.com>
2017-08-04 15:28:52 -0700

    Displays html file on running

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 
 app.get('/', function(req, res) {
-  res.send('Blkjc')
+  fs.readFile('./client/index.html', 'utf8', (err, data) => {
+    if (err) throw err;
+    console.log('Data: ', data);
+    res.send(data);
+  });
 })
 
 app.listen(3000, function() {
