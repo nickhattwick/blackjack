637611c7e575536be5a71617cfaf23952a6f5b6e
Nick <nhattwick@gmail.com>
2017-08-04 18:40:43 -0700

    Stores a deck of playing cards in MongoDB

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
 var express = require('express');
 var fs = require('fs');
 var app = express();
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/myapp', {
+  useMongoClient: true
+});
+var Schema = mongoose.Schema;
+
+var cardSchema = new Schema({
+  id: Number,
+  suit: String,
+  rank: String
+});
+
+var Deck = mongoose.model('Deck', cardSchema);
+var suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades'];
+var ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+
+console.log(ranks);
+var idPlace = 1;
+for (var i = 0; i < suits.length; i++) {
+  for (var j = 0; j < ranks.length; j++) {
+    Deck.create({ id: idPlace, suit: suits[i], rank: ranks[j]});
+    idPlace += 1;
+  }
+}
+
+Deck.find({suit: 'Diamonds'}, 'id suit rank', function(err, card) {
+  if (err) return handleError(err);
+  console.log(card);
+});
 
 app.get('/', function(req, res) {
   fs.readFile('./client/index.html', 'utf8', (err, data) => {
